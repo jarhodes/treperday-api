0e879966d9f68c950ba1e731a55e25fdce331949
jarhodes <github@rhodes.fi>
2020-11-18 16:05:47 +0200

    First commit

---
+++ src/main/java/app/treperday/api/TrePerDayApiApplication.java
@@ -0,0 +1,13 @@
+package app.treperday.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrePerDayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrePerDayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/treperday/api/TrePerDayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.treperday.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrePerDayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
